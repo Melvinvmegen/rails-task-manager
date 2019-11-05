b0b62caf45f937419e176a8edbb00a2a67f7e7ee
melvin van megen <Melvin.vmegen@gmail.com>
2019-11-05 16:16:19 +0100

    02-don

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Read all
+  # get '/tasks', to: 'tasks#index'
+
+  # #  Read one
+  # get '/tasks/:id', to: 'tasks#show', as: :show_task
+
+  # # Create
+  # get '/tasks/new/:id', to: 'tasks#new', as: :new_task
+  # post '/tasks', to: 'tasks#create'
+
+  # # Update
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch '/tasks/:id', to: 'tasks#update'
+  # put '/tasks/:id', to: 'tasks#update'
+
+  resources :tasks
 end
